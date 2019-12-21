93f21dce499a471699d85f101600be9acf57a42f
RockyRose <forthefuture1987@gmail.com>
2019-12-20 16:21:43 -0800

    Adds py script that prints greeting and imports initial libraries to be used

---
+++ python_code.py
@@ -0,0 +1,8 @@
+print('Hello Github')
+#This notebook will be mainly used for the IBM Pro Cert capstone project
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
